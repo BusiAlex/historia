a3a5617911466f7d49beea35ec40152cf01c0a87
BusiAlex <busialex06@gmail.com>
2023-04-20 12:32:25 +0200

    frontend belerakva bele is kezdtem 2023.04.20

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <center><h1>Válassz országot!</h1></center>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card h-100">
+          <img
+            src="../../Images/ZaszloMagyarorszag.png"
+            class="card-img-top"
+            id="my-image"
+            alt="..."
+          />
+          <div class="card-body">
+            <a href="#" class="btn btn-dark" id="my-image">Kiválasztás</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img
+            src="../../Images/ZaszloNemet.png"
+            class="card-img-top"
+            id="my-image"
+            alt="..."
+          />
+          <div class="card-body">
+            <a href="#" class="btn btn-dark" id="my-image">Kiválasztás</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+            <img
+            src="../../Images/ZaszloUSA.png"
+            class="card-img-top"
+            id="my-image"
+            alt="..."
+            />
+          <div class="card-body">
+            <a href="#" class="btn btn-dark" id="my-image">Kiválasztás</a>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Counter from "@/components/Counter.vue";
+import { useCounterStore } from "@/stores/counter";
+const storeCounter = useCounterStore();
+</script>
+
+<style>
+#my-image {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  height: 50%;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+</style>
